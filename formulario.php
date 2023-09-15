5c480b31e12003b61d98ac94523dc9af55610e3f
MagalyTalavera <talaverasanchezmagaly@gmail.com>
2023-09-15 12:06:32 -0500

    s

--- formulario.php
+++ formulario.php
@@ -9,74 +9,54 @@
     <link href="plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
-    <!-- INICIO MENU -->
-    <header id="page-topbar">
-        <div class="navbar-header">
-            <div class="d-flex align-items-left"></div>
-                <div class="d-flex align-items-center">
-                <div class="dropdown d-inline-block ml-2">
-                    <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Usuario
-                        <i class="mdi mdi-chevron-down d-none d-sm-inline-block"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-right">
-                        <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                            Mi Perfil
-                        </a>
-                        <a class="dropdown-item d-flex align-items-center justify-content-between" href="javascript:void(0)">
-                            Cerrar Sesion
-                        </a>
+    <?php 
+    // Lenguaje en php
+    include("include/menu.php");
+
+    ?>
+    <!-- INICIO DE CONTENIDO -->
+    <div class="main-content">
+        <div class="page-content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <form action="" method="POST">
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" > DNI: </label>
+                                        <input type="number" name="dni" class="form-control col-lg-4 col-md-4 col-sm-12">
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" >Apellidos y Nombres:</label>
+                                        <input type="text" class="form-control col-lg-10 col-md-10 col-sm-12" >
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" >Correo:</label>
+                                        <input type="email" class="form-control col-lg-10 col-md-10 col-sm-12" >
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" >Telefono:</label>
+                                        <input type="number" class="form-control col-lg-4 col-md-4 col-sm-12" >
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" >Direccion:</label>
+                                        <input type="text" class="form-control col-lg-10 col-md-10 col-sm-12" >
+                                    </div>
+                                    <div class="form-group row">
+                                        <label class="col-lg-2 col-md-2 col-sm-12" >Fecha de Nacimiento:</label>
+                                        <input type="date" class="form-control col-lg-4 col-md-4 col-sm-12" >
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
                     </div>
-
                 </div>
-
-            </div>
-
-        </div>
-
-    </header>
-
-    <div class="vertical-menu">
-        <div data-simplebar class="h-100">
-            <div class="navbar-brand-box">
-                <a href="" class="logo">
-                    <!-- va el logo, puede ser imagen o texto -->
-                    LOGO
-                </a>
-            </div>
-            <div id="sidebar-menu">
-                <ul class="metismenu list-unstyled" id="side-menu">
-                    <li class="menu-title">MENU</li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="feather-home"></i>Inicio</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="feather-shopping-bag"></i>Productos</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="feather-users"></i>Usuarios</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="feather-truck"></i>Pedidos</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="feather-user-plus"></i>Clientes</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="dripicons-graph-line"></i>Reportes</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="fas fa-users"></i>Proveedores</a>
-                    </li>
-                    <li>
-                        <a href="" class="waves-effect"><i class="dripicons-gear"></i>Mantenimiento</a>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
-    <!-- FIN MENU -->
+     <!-- FIN DE CONTENIDO -->
+
 
     <!-- jQuery  -->
     <script src="plantilla/Admin/vertical/assets/js/jquery.min.js"></script>
