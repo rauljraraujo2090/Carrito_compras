bbc8b90f7c89cf1a010f196d4dad9c3b7eabc610
MagalyTalavera <talaverasanchezmagaly@gmail.com>
2023-09-15 08:13:35 -0500

    s

--- login/index.php
+++ login/index.php
@@ -10,12 +10,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <!-- App favicon -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="/assets/images/login.png">
 
     <!-- App css -->
     <link href="../plantilla/Admin/vertical/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <link href="../plantilla/Admin/vertical/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
     <link href="../plantilla/Admin/vertical/assets/css/theme.min.css" rel="stylesheet" type="text/css" />
+    <link href="../plantilla/Admin/vertical/assets/css/theme.css" rel="stylesheet" type="text/css" />
 
 </head>
 
